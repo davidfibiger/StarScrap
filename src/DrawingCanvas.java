5d06869b1dac4d8e11e2631c5f1417831f317a5b
David Fibiger <davidfibiger03@gmail.com>
2020-01-14 19:54:42 +0100

    Double buffering

--- src/DrawingCanvas.java
+++ src/DrawingCanvas.java
@@ -3,6 +3,8 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 public class DrawingCanvas extends Canvas {
+	private Image dbImage;
+	private Graphics dbg;
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	DrawingCanvas(Frame f){
 		
@@ -12,16 +14,31 @@ public class DrawingCanvas extends Canvas {
 		setSize(f.getWidth() - (f.getInsets().left + f.getInsets().right), f.getHeight() - (f.getInsets().top + f.getInsets().bottom));
 		
 	}
+	public void update(Graphics g) {
+		if(dbImage ==null) {
+			dbImage = createImage(getWidth(), getHeight());
+			dbg = dbImage.getGraphics();
+		}
+		dbg.setColor(getBackground());
+		dbg.fillRect(0, 0, getWidth(), getHeight());		
+		dbg.setColor(getForeground());
+		
+		paint(dbg);
+		g.drawImage(dbImage, 0, 0, this);
+	}
 	public void paint(Graphics g){
-        super.paint(g);
-       
+        //super.paint(g);
+		
         for(int i = 0; i < StarScrapMain.gameStatus.drawables.size(); i++) {
         	Drawable drawable = StarScrapMain.gameStatus.drawables.get(i);
         	drawable.paint(g, StarScrapMain.gameStatus);
         	
         }
-       
+        
 	}
+	/*public void paint() {
+		
+	}*/
 	
 	
 	
